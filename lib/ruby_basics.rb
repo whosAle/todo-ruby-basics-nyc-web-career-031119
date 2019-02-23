60573bbd9f51bfefe92efbafd473d9a705b0cc8a
A Belgrave <abelgrave95@gmail.com>
2019-02-23 21:26:48 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ res = num1/num2
 end
 
 def assign_variable(value)
-  
+  name = value
+
 end
 
-def argue
-  
+def argue(value)
+  value
 end
 
-def greeting
+def greeting(arg1, arg2)
 
 end
 
 def return_a_value
+  "Nice"
 
 end
 
 def last_evaluated_value
+  "Expert"
 
 end
 
-def pizza_party
-
+def pizza_party(val="cheese")
+  val
 end
